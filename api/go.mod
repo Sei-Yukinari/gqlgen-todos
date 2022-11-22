67449d2983b061d1a84d8b9466be9c331291e506
Sei-Yukinari <sei.yukinari@nextremer.com>
2022-11-22 14:51:07 +0900

    imple ws connection

--- api/go.mod
+++ api/go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/mitchellh/mapstructure v1.3.1 // indirect
+	github.com/rs/cors v1.8.2 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/segmentio/ksuid v1.0.4 // indirect
 	github.com/urfave/cli/v2 v2.23.5 // indirect
--- api/server.go
+++ api/server.go
@@ -5,12 +5,16 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/Sei-Yukinari/gqlgen-todos/graph/generated"
 	"github.com/Sei-Yukinari/gqlgen-todos/src/graphql/resolver"
 	"github.com/Sei-Yukinari/gqlgen-todos/src/infrastructure/redis"
+	"github.com/gorilla/websocket"
+	"github.com/rs/cors"
 )
 
 const defaultPort = "8080"
@@ -23,16 +27,29 @@ func main() {
 
 	redis := redis.New(context.Background())
 	rl := resolver.NewResolver(redis)
-	srv := handler.NewDefaultServer(
-		generated.NewExecutableSchema(
-			generated.Config{
-				Resolvers: rl,
+	srv := handler.New(generated.NewExecutableSchema(
+		generated.Config{
+			Resolvers: rl,
+		},
+	))
+
+	srv.AddTransport(transport.Options{})
+	srv.AddTransport(transport.POST{})
+	srv.AddTransport(transport.Websocket{
+		KeepAlivePingInterval: 10 * time.Second,
+		Upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				return true
 			},
-		),
-	)
+		},
+	})
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000", "http://localhost:8080", "ws//localhost:8080"},
+		AllowCredentials: true,
+	})
+	http.Handle("/query", c.Handler(srv))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
