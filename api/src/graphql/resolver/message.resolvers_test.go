17ff23076e78d883989e4f610eb8955e01dc71a7
Sei-Yukinari <sei.yukinari@nextremer.com>
2022-12-01 16:52:31 +0900

    add resolver(Messages) test

--- api/src/graphql/resolver/message.resolvers_test.go
+++ api/src/graphql/resolver/message.resolvers_test.go
@@ -1,6 +1,8 @@
 package resolver_test
 
 import (
+	"context"
+	"sort"
 	"testing"
 	"time"
 
@@ -45,4 +47,52 @@ func Test_mutationResolver_PostMessage(t *testing.T) {
 		assert.Equal(t, expected.Text, actual.Text)
 		assert.Equal(t, expected.User, actual.User)
 	})
+}
+
+func Test_queryResolver_Messages(t *testing.T) {
+	r := test.NewResolverMock(t, mysqlContainer, redisContainer)
+	c := test.NewGqlgenClient(r)
+	ctx := context.Background()
+	messages := []*model.Message{
+		{
+			ID:        "1",
+			CreatedAt: time.Now().UTC(),
+			User:      "Dummy User1",
+			Text:      "Dummy Text1",
+		},
+		{
+			ID:        "2",
+			CreatedAt: time.Now().UTC(),
+			User:      "Dummy User2",
+			Text:      "Dummy Text2",
+		},
+	}
+	r.Repositories.Message.PostAndPublish(ctx, messages[0])
+	r.Repositories.Message.PostAndPublish(ctx, messages[1])
+	var res struct {
+		Messages []*gmodel.Message
+	}
+	c.MustPost(`
+			query findMessages {
+			  messages {
+				id
+				user
+				text
+			  }
+}`,
+		&res,
+		client.Var("input", gmodel.PostMessageInput{
+			User: "Dummy User",
+			Text: "Dummy Text",
+		}),
+	)
+	actual := res.Messages
+	expected := r.Presenter.Messages(messages)
+	sort.Slice(expected, func(i, j int) bool {
+		return expected[i].ID > expected[j].ID
+	})
+	assert.Equal(t, expected[0].ID, actual[0].ID)
+	assert.Equal(t, expected[0].User, actual[0].User)
+	assert.Equal(t, expected[0].Text, actual[0].Text)
+	assert.Equal(t, len(expected), len(actual))
 }
