029a727a4a4ba4d82e50c761cc6fb98ecb508a35
Sei-Yukinari <sei.yukinari@nextremer.com>
2022-12-08 15:42:15 +0900

    remove trash

--- api/test/init/users.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `users`
-(
-    `id`   int unsigned                            NOT NULL AUTO_INCREMENT,
-    `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 10
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_general_ci;
--- api/test/migrations/000001_create_users_table.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `users`
-(
-    `id`   int unsigned NOT NULL AUTO_INCREMENT,
-    `name` varchar(255) NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_general_ci;
